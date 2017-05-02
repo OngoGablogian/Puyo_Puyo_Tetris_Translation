28ffb239b0542d3105c68fcd1d5641183fb200ea
DarkSynopsis <gunnut14@gmail.com>
2017-05-03 07:06:51 +1200

    Rebuilding DLC Instructions + Act 8
    Instructions for rebuilding the DLC along with some batch files to make the process easier.
    All Act 8 Scenes done, need to do the little messages after finishing a match (vs_adv_08Japanese) then Act 8 should be finished.

---
+++ Translated DLC/4_Build_DLC.bat
@@ -0,0 +1,3 @@
+3dstool -cvtf romfs contents_0010_00000010/romfs.bin --romfs-dir contents_0010_00000010/romfs
+3dstool -cvtf cfa contents.0010.00000010 --header contents_0010_00000010/ncch.header --romfs contents_0010_00000010/romfs.bin
+DEL contents_0010_00000010\romfs.bin
