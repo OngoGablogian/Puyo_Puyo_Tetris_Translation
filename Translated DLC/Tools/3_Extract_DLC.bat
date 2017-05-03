c933a333721e0ba11d6aeaba1dee072d61b4bd6e
DarkSynopsis <gunnut14@gmail.com>
2017-05-03 17:55:18 +1200

    Act 9 Complete
    Act 9 Complete.
    Edit to BAT files to build it.

--- Translated DLC/Tools/3_Extract_DLC.bat
+++ Translated DLC/Tools/3_Extract_DLC.bat
@@ -2,3 +2,8 @@ md contents_0010_00000010
 3dstool -xvtf cfa contents.0010.00000010 --header contents_0010_00000010/ncch.header --romfs contents_0010_00000010/romfs.bin
 3dstool -xvtf romfs contents_0010_00000010/romfs.bin --romfs-dir contents_0010_00000010/romfs
 DEL contents_0010_00000010\romfs.bin
+
+md contents_0011_00000011
+3dstool -xvtf cfa contents.0011.00000011 --header contents_0011_00000011/ncch.header --romfs contents_0011_00000011/romfs.bin
+3dstool -xvtf romfs contents_0011_00000011/romfs.bin --romfs-dir contents_0011_00000011/romfs
+DEL contents_0011_00000011\romfs.bin
